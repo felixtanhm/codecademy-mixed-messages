9fac0d0c88b8d3f0a8f15836d0db2c5b0748a485
Felix Tan <felixtanhm@gmail.com>
2023-05-28 13:54:14 +0800

    add message component variables

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
-console.log('test merge 2')
-console.log(`test merge`)
+const messages = ["message1","message2","message3","message4","message5","message6","message7","message8","message9","message10"]
+const name = ["name1", "name2", "name3", "name4", "name5"]
+const lastName = ["last1", "last2", "last3", "last4", "last5"]
+
+
